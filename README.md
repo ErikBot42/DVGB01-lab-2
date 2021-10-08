a960175c1a7fb5c8800a066fd462170c52f27937
ErikBot42 <erik.arn.magnusson@gmail.com>
2021-10-08 21:08:54 +0200

    fixes from new instructions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DVGB01-lab-2
+
+Requires python3
+
+'chmod +x sched.py' and './sched.py' (assuming '/usr/bin/python3' containa a python3 installation)
+
+or 'python3 sched.py'
--- sched.py
+++ sched.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 import sys
 import operator
 import copy
@@ -13,10 +14,11 @@ def print_usage():
 
 algorithms = ["FCFS", "SJF", "RR"]
 
-i = 0
+# read CLI arguments
 filename = ""
 algorithm = algorithms[0]
 timeq = 5
+i = 0
 while i<len(sys.argv):
     curr = sys.argv[i];i+=1
     if curr == "-f":
@@ -35,44 +37,60 @@ print("Algorithm:",algorithm)
 print("Time quanta:",timeq)
 print("-------------------")
 
+# waiting time = turnaround - execute time
+
+def num_format(number):
+    return '{0: <2}'.format(str(number))
+
 class Process:
     pid=0
     arrival=0
     time_left=0
     time_total_needed=0
-
-    time_at_last_execute=0 # when last wait started
+   
+    time_at_done=0 # time at last stopped processing
     waiting_time=0
 
-    start_time=-1
+    #start_time=-1
 
+    # turnaround = from arrival to done
     def turnAroundTime(self):
-        return self.time_at_last_execute-self.start_time
+        return self.time_at_done-self.arrival
 
     def __init__(self, line):
         vec = line.split(",")
-        self.pid = int(vec[0])
+        self.pid = int(vec[0]) # input file is pid, arrival, time needed
         self.arrival = int(vec[1])
         self.time_total_needed = int(vec[2])
         self.time_left = self.time_total_needed
-        self.time_at_last_execute = self.arrival
+        self.time_at_done = self.arrival
 
     def print(self):
-        print("PID:", self.pid," Arrival time:", self.arrival,"ms  Time left:", self.time_left,"/",self.time_total_needed,"ms") 
-        print("    Waiting time:", self.waiting_time,"ms Turaround time:", self.turnAroundTime()) 
+        RED = '\033[91m'
+        BOLD = '\033[1m'
+        END = '\033[0m'
+        print("PID:", num_format(self.pid)," Arrival time:", num_format(self.arrival),"ms  Time left:", num_format(self.time_left),"/",num_format(self.time_total_needed),"ms") 
+        print(BOLD,"    Waiting time:",RED, num_format(self.waiting_time),"ms",END,BOLD,"Turaround time:",RED, num_format(self.turnAroundTime()),"ms",END) 
+        print()
 
     def execute(self, currentTime, executionTime):
-        self.waiting_time+=currentTime-self.time_at_last_execute
-        self.time_at_last_execute=currentTime+executionTime
+        self.waiting_time+=currentTime-self.time_at_done
+        self.time_at_done=currentTime+executionTime
         self.time_left-=executionTime
-        if self.start_time < 0:
-            self.start_time = currentTime
+        #if self.start_time < 0:
+        #    self.start_time = currentTime
 
         print("Running PID:", self.pid,"for",executionTime,"ms  at time",currentTime)
 
 processes = []
 
+def process_wait(targetTime, currentTime):
+    time = targetTime-currentTime
+    print("Waiting until", targetTime, "ms  for", time, "ms  at time",currentTime)
+    return time+currentTime
 
+def too_late(process):
+    print("PID", process.pid,"is too late to be run")
 
 with open(filename) as file:
     lines = file.readlines()
@@ -88,18 +106,30 @@ if algorithm == "FCFS":
     byArrivalTime= sorted(processes, key=operator.attrgetter("arrival"))
     for process in byArrivalTime:
         executionTime = process.time_left
+        if process.arrival>currentTime:
+            currentTime = process_wait(process.arrival, currentTime)
         process.execute(currentTime, executionTime)
         currentTime+=executionTime
 elif algorithm == "SJF":
     byBurstTime = sorted(processes, key=operator.attrgetter("time_total_needed"))
-    for process in byBurstTime:
-        executionTime = process.time_left
-        process.execute(currentTime, executionTime)
-        currentTime+=executionTime
+    processSet = copy.copy(byBurstTime)
+    while len(processSet)>0:
+        for process in processSet:
+            if process.arrival>currentTime:
+                too_late(process)
+            else:
+                executionTime = process.time_left
+                process.execute(currentTime, executionTime)
+                currentTime+=executionTime
+                break
+        processSet = [p for p in processSet if p.time_left>0]
+
 elif algorithm == "RR":
     processSet = copy.copy(processes)
     while len(processSet)>0:
         for process in processSet:
+            if process.arrival>currentTime:
+                continue
             executionTime = min(timeq,process.time_left)
             process.execute(currentTime, executionTime)
             currentTime+=executionTime
@@ -107,6 +137,7 @@ elif algorithm == "RR":
         processSet = [p for p in processSet if p.time_left>0]
 
 print("-------------------")
+print()
 for process in processes:
     process.print()
 print("-------------------")
